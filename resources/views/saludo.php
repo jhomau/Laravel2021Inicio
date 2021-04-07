b1e96200135b71b1e1517ebeedddfa611c413f8f
jhomau <jhomau.666@gmail.com>
2021-04-07 21:41:08 +0200

    P/7/Abril/21 || Redireccionamiento de vistas simples en formato HTML || manejo de alias en rutas para redireccionamiento por HTML con php

---
+++ resources/views/saludo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saludo</title>
+</head>
+<body>
+    <h1>Saludos para <?php echo $nombre; ?></h1> <!--pasar es el nombre con el que llega desde la ruta y se recupera aqui-->
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,31 @@ use Illuminate\Support\Facades\Route;
 
 /*P/7/ABRIL/21 */
 /*Inicio Laravel rutas de laravel y uso de alias de rutas y envio por rutas*/
-Route::get('/',function(){
+Route::get('/links',function(){
     echo "<a href=". route('contacto').">Contactos</a><br>";
     echo "<a href=". route('contacto').">Contactos</a><br>";
     echo "<a href=". route('contacto').">Contactos</a><br>"; //Usando alias cambiamos la ruta y se mantiene el codigo
     echo "<a href='contacto'>Contactos</a><br>"; // No funciona por que no usa alias sino una ruta antigua
 });
+/*Route::get('contactame',['as'=>'contacto',function(){
+    return "Hola desde pagina contacto";
+}]);
+/* Route::get('saludo/{nombre?}',function($nombre = "invitado"){ //Se usa ? para poder usar un nombre por defecto en caso que no se use ningun nombre
+    return "Saludos $nombre";
+})->where('nombre',"[A-Za-z]+");//expresion regular para que no se use numeros
+*/
+
+/*P/7/ABRIL/21 */
+/* Manejo de vistas de HTML simples || Uso de alias para redireccionamiento de rutas*/
+Route::get('/',['as'=>'home',function(){
+    return view('home');
+}]);
+Route::get('saludo/{nombre?}',['as'=>'saludo',function($nombre = "Invitado"){
+    //return view('saludo',['nombre'=>$nombre]); // Otra forma de enviar parametro por ruta
+    //return view('saludo')->with(['pasar'=>$nombre]); // pasar es el nombre para la recupera en la vista el dato de NOMBRE o lo que se envie por ruta
+    return view('saludo',compact('nombre'));
+}]);
 Route::get('contactame',['as'=>'contacto',function(){
     return "Hola desde pagina contacto";
 }]);
-Route::get('saludo/{nombre?}',function($nombre = "invitado"){ //Se usa ? para poder usar un nombre por defecto en caso que no se use ningun nombre
-    return "Saludos $nombre";
-})->where('nombre',"[A-Za-z]+");//expresion regular para que no se use numeros
 /* */
