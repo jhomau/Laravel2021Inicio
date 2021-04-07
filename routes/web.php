027d140e167ccf6bacf6ce7a51b380798b4bd89e
jhomau <jhomau.666@gmail.com>
2021-04-07 20:34:08 +0200

    P/7/Abril/21 || Inicio Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+/*P/7/ABRIL/21 */
+/*Inicio Laravel rutas de laravel y uso de alias de rutas y envio por rutas*/
+Route::get('/',function(){
+    echo "<a href=". route('contacto').">Contactos</a><br>";
+    echo "<a href=". route('contacto').">Contactos</a><br>";
+    echo "<a href=". route('contacto').">Contactos</a><br>"; //Usando alias cambiamos la ruta y se mantiene el codigo
+    echo "<a href='contacto'>Contactos</a><br>"; // No funciona por que no usa alias sino una ruta antigua
 });
-
-
+Route::get('contactame',['as'=>'contacto',function(){
+    return "Hola desde pagina contacto";
+}]);
+Route::get('saludo/{nombre?}',function($nombre = "invitado"){ //Se usa ? para poder usar un nombre por defecto en caso que no se use ningun nombre
+    return "Saludos $nombre";
+})->where('nombre',"[A-Za-z]+");//expresion regular para que no se use numeros
+/* */
